8057bacab8379ca0fd6cf1cd0318751dba892c47
EgorPlat <egorplatonov2002@gmail.com>
2023-08-21 13:17:36 +0300

    inithial

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,11 @@
+import s from './ProductCard.module.scss';
+
+function ProductCard() {
+  return (
+    <div className={s.wrapper}>
+        test
+    </div>
+  );
+}
+
+export default ProductCard;
